87355e694d01f7acba87a4020a95238f8d5154ca
Jocke <jocke17h@hotmail.com>
2019-05-18 23:26:18 +0200

    Updated materialUi, added TableClass to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
-import table from './game/Table.js';
-
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
+import TableClass from './game/TableClass.js';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header"> 
+      <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
+          <TableClass />
         
       </header>
     </div>
