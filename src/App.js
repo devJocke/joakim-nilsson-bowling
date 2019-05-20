148f180d7ada471123f1f4f52c6b04205cbd5066
Jocke <jocke17h@hotmail.com>
2019-05-20 11:04:17 +0200

    Randomized value presentation for user
    Player 1 can now see the randomized number.
    TODO:: Half/Strike, Miss

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
         <h3>Scored XXX</h3>
 
-        <Button id="playButton" variant="contained" color="primary" onClick={randomiseNumber}>
+        <Button id="playButton" variant="contained" color="primary" onClick={randomizeNumber}>
           Player 1
         </Button>
 
@@ -25,37 +25,47 @@ function App() {
   );
 }
 
+let currentPlayer = 1;
 let tenpinsLeft = 10;
 let throwCounter = 1;
-let currentPlayer = 1;
+let roundCounter =1; 
 
-function randomiseNumber() {
+function randomizeNumber() {
   //Math.random begins at 0 so we need to increase number +1 to be able to miss the shots entirely
-  tenpinsLeft = Math.floor(Math.random() * (tenpinsLeft + 1));
+  console.log(tenpinsLeft); 
 
+  tenpinsLeft =  Math.floor(Math.random() * (tenpinsLeft + 1)); 
   //If all tenpins are down check if its the first or second throw
   if (tenpinsLeft === 0) {
     //If its the first then a strike has been made
     if (throwCounter === 1) {
       console.log("PLAYER " + currentPlayer + " STRIKE    " + tenpinsLeft);
+      document.getElementById(roundCounter + throwCounter).innerHTML ="X" 
     }
     //If its the first then a half-strike has been made
     else if (throwCounter === 2) {
       console.log("PLAYER " + currentPlayer + " HALF-STRIKE    " + tenpinsLeft);
+      document.getElementById(roundCounter + throwCounter).innerHTML ="/" 
     }
     //Proceed by resetting the tenpins and change to the next player
     checkPlayerTurn();
   }
   //If no half/strike has been made proceed displaying the remaining tenpins
   else if (throwCounter === 1) {
-    console.log("PLAYER " + currentPlayer + " FIRST BLOW     " + tenpinsLeft + " LEFT");
+    console.log("PLAYER " + currentPlayer + " FIRST BLOW     " + tenpinsLeft + " DOWN"); 
+    document.getElementById(roundCounter+ throwCounter).innerHTML =   tenpinsLeft;
     throwCounter = 2;
+    
   }
   //If the player is on his second throw proceed by resetting the tenpins and change to the next player
   else {
-    console.log("PLAYER " + currentPlayer + " BLOW 2     " + tenpinsLeft + " LEFT");
+    console.log("PLAYER " + currentPlayer + " BLOW 2     " + tenpinsLeft + " DOWN");
+    document.getElementById(roundCounter+ throwCounter).innerHTML =  tenpinsLeft;
     checkPlayerTurn();
-  }
+  } 
+
+  //Set the remaning pins for new randomization
+  tenpinsLeft = 10 - tenpinsLeft;  
   //TODO MISSA
 }
 
@@ -67,7 +77,7 @@ function checkPlayerTurn() {
     currentPlayer = 1;
     document.getElementById("playButton").innerText = "player 1";
   }
-  tenpinsLeft = 10;
   throwCounter = 1;
+  roundCounter++;
 }
 export default App;
