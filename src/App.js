0d8c06f7d02e9253591b9477fcb7f3c3df2d5af2
Jocke <jocke17h@hotmail.com>
2019-05-21 11:04:15 +0200

    Moved logic into models

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import BowlingTable from './indexComponents/BowlingTable.js';
 import Button from '@material-ui/core/Button';
-import { randomizeNumber } from './HandlePlayers';
+import { randomizeNumber } from './models/HandlePlayers';
 
 function App() {
 
