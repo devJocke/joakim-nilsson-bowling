eaec2af01ed4e7f3768fccc9b0bbef86c4bc610f
Jocke <jocke17h@hotmail.com>
2019-05-27 22:01:04 +0200

    HandlePlayers is now a class object
    Handleplayers can now be instantiated several times and lives on its
    own.
    This creates dynamic, for example if we would like to add 4 players
    instead.
    Then instantiate 4 objects and bind them so their respective
    bowlingtable and roll button.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React from 'react';
 import './App.css';
 import BowlingTable from './indexComponents/BowlingTable.js';
 import Button from '@material-ui/core/Button';
-import { randomizeNumber } from './models/HandlePlayers';
+import HandlePlayers from './models/HandlePlayers';
 
 function App() {
 
+  let player1 = new HandlePlayers();
+  let player2 = new HandlePlayers();
+
+  window.onload = function(){
+    document.getElementById("playButton2").innerText = "player 2" 
+    document.getElementById("playButton2").disabled = true; 
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
 
         <h3>Scored XXX</h3>
-
-        <Button id="playButton" variant="contained" color="primary" onClick={randomizeNumber}>
+        <Button id="playButton1" variant="contained" color="primary" onClick={() => player1.randomizeNumber("player1")}>
           Player 1
         </Button>
-
         <h3>Player 1</h3>
         <BowlingTable player="player1" />
+
+        <Button id="playButton2" variant="contained" color="primary" onClick={() => player2.randomizeNumber("player2")}>
+          Player 2
+        </Button>
 
         <h3>Player 2</h3>
         <BowlingTable player="player2" />
