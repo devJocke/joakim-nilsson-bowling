de6dee9f05b18179bb76266999e15186050c5e88
Jocke <jocke17h@hotmail.com>
2019-05-28 11:12:20 +0200

    Fade component, foreground
    Fade added when entering the website
    Foreground added for p1 and p2 that animates respective players table

--- src/App.js
+++ src/App.js
@@ -3,36 +3,50 @@ import './App.css';
 import BowlingTable from './indexComponents/BowlingTable.js';
 import Button from '@material-ui/core/Button';
 import HandlePlayers from './models/HandlePlayers';
+import { Fade } from '@material-ui/core';
 
 function App() {
 
   let player1 = new HandlePlayers();
   let player2 = new HandlePlayers();
 
-  window.onload = function(){
-    document.getElementById("playButton2").innerText = "player 2" 
-    document.getElementById("playButton2").disabled = true; 
+  window.onload = function () {
+    document.getElementById("playButton2").innerText = "Player 2 Throw"
+    document.getElementById("playButton2").disabled = true;
   }
-  
+
   return (
     <div className="App">
       <header className="App-header">
 
-        <h3>Scored XXX</h3>
-        <Button id="playButton1" variant="contained" color="primary" onClick={() => player1.randomizeNumber("player1")}>
-          Player 1
-        </Button>
-        <h3>Player 1</h3>
-        <BowlingTable player="player1" />
+        <Fade in={true} timeout={500} style={{ width: "100%" }} >
+          <div>
+            <h3 id="gameInfoDisplay">Scored XXX</h3>
 
-        <Button id="playButton2" variant="contained" color="primary" onClick={() => player2.randomizeNumber("player2")}>
-          Player 2
-        </Button>
+            <Button id="playButton1" variant="contained" color="primary" onClick={() => player1.randomizeNumber("player1")}>
+              Player 1 Throw
+            </Button>
 
-        <h3>Player 2</h3>
-        <BowlingTable player="player2" />
+            <h3>Player 1</h3>
+
+            <div id="player1Foreground" className="foreground">
+              <BowlingTable player="player1" />
+            </div>
+
+            <Button id="playButton2" variant="contained" color="primary" onClick={() => player2.randomizeNumber("player2")} >
+              Player 2 Throw
+           </Button>
+
+            <h3>Player 2</h3>
+
+            <div id="player2Foreground" className="foreground" >
+              <BowlingTable player="player2" />
+            </div>
+            
+          </div>
+        </Fade >
       </header>
-    </div>
+    </div >
   );
 }
 
